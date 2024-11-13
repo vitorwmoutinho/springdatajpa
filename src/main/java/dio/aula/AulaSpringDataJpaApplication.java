99e1a94dc415853f001f0d76d208dc0ac25acd8d
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-13 14:21:25 -0300

    Aprendendo a utilizar o spring data JPA

--- src/main/java/dio/aula/AulaSpringDataJpaApplication.java
+++ src/main/java/dio/aula/AulaSpringDataJpaApplication.java
@@ -8,6 +8,8 @@ public class AulaSpringDataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaSpringDataJpaApplication.class, args);
+		//inicializacao
+
 	}
 
 }
--- src/main/java/dio/aula/StartApp.java
+++ src/main/java/dio/aula/StartApp.java
@@ -6,18 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class StarApp implements CommandLineRunner {
+public class StartApp implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
+
     @Override
     public void run(String... args) throws Exception {
-        User user = new User();
-        user.setName("Gleyson");
-        user.setUsername("glysns");
-        user.setPassword("Dio123");
+        List<User> users = repository.findByNameContaining("GLEYSON");
 
-        for(User u: repository.findAll()){
+
+        User user = new User();
+        user.setName("GABRIEL NUNES");
+        user.setUsername("gabriel");
+        user.setPassword("santos");
+        repository.save(user);
+
+        for (User u : repository.findAll()) {
             System.out.println(u);
         }
 
